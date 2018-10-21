a15d9155c507a8b9401d8e8e24b1e973f31e5f84
Ryan Stillings <rystills@gmail.com>
2018-10-21 17:02:14 -0400

    line rendering at mouse positions

---
+++ Assets/Scripts/drawPath.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drawPath : MonoBehaviour {
+	List<Vector2> points = new List<Vector2>();
+	public Material lineMat;
+	LineRenderer lineRenderer;
+
+	private void Start() {
+		lineRenderer = gameObject.GetComponent<LineRenderer>();
+		//lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+		lineRenderer.widthMultiplier = 0.2f;
+
+		float alpha = 1.0f;
+		Gradient gradient = new Gradient();
+		gradient.SetKeys(
+			new GradientColorKey[] { new GradientColorKey(Color.yellow, 0.0f), new GradientColorKey(Color.red, 1.0f) },
+			new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+			);
+		lineRenderer.colorGradient = gradient;
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 scaledMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			points.Add(new Vector2(scaledMousePos.x, scaledMousePos.y));
+		}
+		lineRenderer.positionCount = points.Count;
+		for (int i = 0; i < points.Count; ++i) {
+			lineRenderer.SetPosition(i, points[i]);
+		}
+	}
+}
---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, 0, 60 * Time.deltaTime);
+		transform.Translate(new Vector2(2 * Time.deltaTime, 3 * Time.deltaTime), Space.World);
+	}
+}
