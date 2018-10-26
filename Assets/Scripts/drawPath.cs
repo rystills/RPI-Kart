709d5e3643ae2689fc441f4b4ba69654898eeec5
VinnyF <vincentferraraiii@gmail.com>
2018-10-25 20:39:17 -0400

    line slides along walls
    kinda sorta

--- Assets/Scripts/drawPath.cs
+++ Assets/Scripts/drawPath.cs
@@ -56,9 +56,30 @@ public class drawPath : MonoBehaviour {
 							finalPoint.x -= Mathf.Cos(ang) * (.001f);
 							finalPoint.y -= Mathf.Sin(ang) * (.001f);
 						}
-						points.Add(finalPoint);
-						//TODO: try resolving collisions on each individual axis
-					}
+
+                        //Let line drawer slide along walls
+                        //Any extra collisions this causes seems to correct itself on the next loop
+                        ang = Mathf.Atan2((scaledMousePos.y - oldPoint.y), (scaledMousePos.x - oldPoint.x));
+                        if (ang < 0) ang += 2 * Mathf.PI;
+                        if (ang >= 0 && ang < Mathf.PI / 2) { //first quadrant
+                            finalPoint.x += .01f;
+                            finalPoint.y += .01f;
+                        }
+                        else if (ang >= Mathf.PI / 2 && ang < Mathf.PI) { //second quadrant
+                            finalPoint.x -= .01f;
+                            finalPoint.y += .01f;
+                        }
+                        else if (ang >= Mathf.PI && ang < 3 * Mathf.PI / 2) { //third quadrant
+                            finalPoint.x -= .01f;
+                            finalPoint.y -= .01f;
+                        }
+                        else { //fourth
+                            finalPoint.x += .01f;
+                            finalPoint.y -= .01f;
+                        }
+                        points.Add(finalPoint);
+                        //TODO: try resolving collisions on each individual axis
+                    }
 				}
 			}
 		}
