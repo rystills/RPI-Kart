fd8f4c6bc66bc4f6ed8c2ad790b601672397c839
Ryan Stillings <rystills@gmail.com>
2018-10-30 15:19:35 -0400

    user can now press space to freeze/unfreeze time

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public bool timeFrozen = false;
+
+	void Update () {
+		//freeze/unfreezes time on spacebar
+		if (Input.GetKeyDown("space")) {
+			timeFrozen = !timeFrozen;
+			Time.timeScale = timeFrozen ? 0 : 1;
+		}
+	}
+}
