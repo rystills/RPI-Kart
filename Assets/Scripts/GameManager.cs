974cdcb9306e1f284f1999d0d15f0ab93cd0525a
Ryan Stillings <rystills@gmail.com>
2018-11-02 13:42:40 -0400

    use a prefab for walls to simplify wall generation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour {
 	public Transform debugPoint;
 	public Texture wallTex;
 	public Material[] floorMats;
-	public Shader wallShader;
+	public Transform wallPrefab;
 
 	private void Start() {
 		//quick and dirty map load (sorry guys, Unity didn't seem to support loading JSON into a generic Dict?)
@@ -80,28 +80,17 @@ public class GameManager : MonoBehaviour {
 		//generate map walls
 		for (int i = 0; i < edgesPos.Count; ++i) {
 			float vertDist = Vector2.Distance(vertsPos[(int)edgesPos[i][0]], vertsPos[(int)edgesPos[i][1]]);
-			Vector2 p1 = vertsPos[(int)edgesPos[i][1]];
-			Vector2 p0 = vertsPos[(int)edgesPos[i][0]];
+			Vector2 p1 = vertsPos[(int)edgesPos[i][1]], p0 = vertsPos[(int)edgesPos[i][0]];
 			float vertAng = Mathf.Atan2(p1.y - p0.y, p1.x - p0.x);
 			Vector2 center = new Vector2((p1.x + p0.x) / 2, (p1.y + p0.y)/2);
 			//store mesh data and collider in separate objects as plane needs to rotate 90 degrees to face camera, which breaks collider
-			GameObject wallParent = new GameObject();
-			GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Plane);
-			wallParent.name = "Wall"+i;
-			wall.name = "wall mesh";
-			wall.transform.parent = wallParent.transform;
-			wall.transform.rotation *= Quaternion.Euler(-90, 0, 0);
-			wall.transform.localScale = new Vector3(vertDist * .1f, 1, .01f);
-			DestroyImmediate(wall.GetComponent<MeshCollider>());
-			wallParent.layer = 11;
-			BoxCollider2D coll = wallParent.AddComponent<BoxCollider2D>();
-			MeshRenderer renderer = wall.GetComponent<MeshRenderer>();
-			renderer.material.shader = wallShader;
-			renderer.material.mainTexture = wallTex;
-			renderer.material.mainTextureScale = new Vector2(10,.1f);
-			wallParent.transform.rotation *= Quaternion.Euler(0, 0,vertAng*180/Mathf.PI);
-			wallParent.transform.position = center;
+			Transform wallParent = Instantiate(wallPrefab);
+			Transform wall = wallParent.transform.GetChild(0);
+			wall.localScale = new Vector3(vertDist * .1f, 1, .01f);
+			BoxCollider2D coll = wallParent.GetComponent<BoxCollider2D>();
 			coll.size = new Vector2(vertDist,.1f);
+			wallParent.rotation *= Quaternion.Euler(0, 0, vertAng * 180 / Mathf.PI);
+			wallParent.position = center;
 		}
 
 		//generate map floors
