595bb85928f37a034973388ef5b67ce9ac7b1c69
VinnyF <vincentferraraiii@gmail.com>
2018-12-05 21:57:03 -0500

    pushing work so far

--- Assets/Scripts/gui.cs
+++ Assets/Scripts/gui.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class gui : MonoBehaviour {
 
     Texture2D guiBGText;
-
+    Texture playerText;
+    
     // Use this for initialization
     void Start () {
         guiBGText = new Texture2D(1, 1);
         guiBGText.SetPixel(0, 0, new Color(.117f, .29f, .29f, .95f));
         guiBGText.wrapMode = TextureWrapMode.Repeat;
-        guiBGText.Apply();        
+        guiBGText.Apply();
+
+        //Sprite[] playerSprite = Resources.LoadAll<Sprite>("Images/human1");
+        //playerText = playerSprite[0].Texture;
     }
 	
 	// Update is called once per frame
@@ -22,5 +26,6 @@ public class gui : MonoBehaviour {
     void OnGUI() {
         Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
         GUI.DrawTexture(new Rect(0, 0, screenPos.x * 2, 20), guiBGText);
+        //GUI.DrawTexture(new Rect(0, 0, screenPos.x * 2, 20), playerText);
     }
 }
