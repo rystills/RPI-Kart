7eb3b869c04008bb5bfcebaa3e50b9c850480407
Jordan <jallibad@gmail.com>
2018-12-10 17:33:56 -0500

    randomy generate main menu bg texture for more visual variety

--- Assets/Scripts/drawMainMenu.cs
+++ Assets/Scripts/drawMainMenu.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class drawMainMenu : MonoBehaviour {
 	GUIStyle menuButtonStyle;
 	GUIStyle centeredStyle = null;
+	Texture2D texture;
 
 	private void Start() {
 		menuButtonStyle = new GUIStyle("button");
 		menuButtonStyle.fontSize = 24;
+
+		//create random gradient texture at runtime
+		texture = new Texture2D(3, 3, TextureFormat.ARGB32, false);
+		for (int i = 0; i < 9; ++i) {
+			texture.SetPixel(i%3, (int)(i/3), new Color(Random.Range(0f,1f),Random.Range(0f,1f),Random.Range(0f,1f)));
+		}
+		texture.filterMode = FilterMode.Trilinear;
+		texture.Apply();
 	}
 
 	void OnGUI() {
 		//GUI functions can only run in OnGUI, because...Unity?
+		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),texture,ScaleMode.ScaleAndCrop,true);
 		if (centeredStyle == null) {
 			centeredStyle = new GUIStyle(GUI.skin.label);
 			centeredStyle.alignment = TextAnchor.UpperCenter;
