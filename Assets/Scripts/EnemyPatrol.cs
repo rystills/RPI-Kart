9a5e29f16f90c91f6e92988c347b0bd7610c48e6
yungkyung <tchang422@gmail.com>
2018-12-11 21:15:35 -0500

    Added enemy patrolling behaviour and more artwork

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour {
+
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+    public Transform[] moveSpots;
+    private int startingSpot;
+	// Use this for initialization
+	void Start () {
+        // Initialize the wait time for the pathing to start
+        waitTime = startWaitTime;
+
+        // Choose a random starting spot
+        startingSpot = Random.Range(0, moveSpots.Length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[startingSpot].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots[startingSpot].position) < 0.2f);
+            if(waitTime <= 0)
+        {
+            startingSpot = Random.Range(0, moveSpots.Length);
+            waitTime = startWaitTime;
+        }
+        else
+        {
+            waitTime -= Time.deltaTime;
+        }
+    }
+}
