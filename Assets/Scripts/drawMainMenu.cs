fa7daae227845cad214c01b13ab45869f65832b8
Ryan Stillings <rystills@gmail.com>
2018-12-12 17:29:39 -0500

    bring back random main menu texture (seems to have been lost during a merge)

--- Assets/Scripts/drawMainMenu.cs
+++ Assets/Scripts/drawMainMenu.cs
@@ -5,24 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class drawMainMenu : MonoBehaviour {
     GUIStyle centeredStyle = null;
+	Texture2D texture;
 
-    float native_width = 1920;
+	float native_width = 1920;
     float native_height = 1080;
 
     private void Start() {
-
-    }
+		//create random gradient texture at runtime
+		texture = new Texture2D(3, 3, TextureFormat.ARGB32, false);
+		for (int i = 0; i < 9; ++i) {
+			texture.SetPixel(i % 3, (int)(i / 3), new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+		}
+		texture.filterMode = FilterMode.Trilinear;
+		texture.Apply();
+	}
 
     void OnGUI() {
-        float rx = Screen.width / native_width;
+		//update texture colors by random amounts
+		for (int i = 0; i < 9; ++i) {
+			Color curColor = texture.GetPixel(i % 3, (int)(i / 3));
+			texture.SetPixel(i % 3, (int)(i / 3), new Color(curColor.r + Random.Range(-.002f, .002f), curColor.g + Random.Range(-.002f, .002f), curColor.b + Random.Range(-.002f, .002f)));
+		}
+		texture.Apply();
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), texture, ScaleMode.ScaleAndCrop, true);
+
+		float rx = Screen.width / native_width;
         float ry = Screen.height / native_height;
         GUI.matrix = Matrix4x4.TRS(new Vector3(0, 0, 0), Quaternion.identity, new Vector3(rx, ry, 1));
 
         GUIStyle button = new GUIStyle("button");
         button.fontSize = 24;
 
-        //GUI functions can only run in OnGUI, because...Unity?
-        if (centeredStyle == null) {
+		//GUI functions can only run in OnGUI, because...Unity?
+		if (centeredStyle == null) {
             centeredStyle = new GUIStyle(GUI.skin.label);
             centeredStyle.alignment = TextAnchor.UpperCenter;
             centeredStyle.fontSize = 128;
